0dba1d1111c8cb03ae3bc69f461d8271d2f938a0
Wilfredo-hue <78829758+Wilfredo-hue@users.noreply.github.com>
2021-02-12 17:30:59 -0600

    Guardo Progra.

---
+++ Wilfredo01/Wilfredo01/VISTA/FrmLoguin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wilfredo01
+{
+    public partial class txtUSer : Form
+    {
+        public txtUSer()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLoguin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
