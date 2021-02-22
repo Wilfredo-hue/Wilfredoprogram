5f440414095049eb7543041682992fd96f09aa93
Wilfredo-hue <78829758+Wilfredo-hue@users.noreply.github.com>
2021-02-21 18:14:17 -0600

    Creación de Proyectos

---
+++ Historial De Ejercicios/Wilfredo01/Wilfredo01/DOMINIO/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wilfredo01.NEGOCIO;
+namespace Wilfredo01.DOMINIO
+{
+    class LOG_IN
+    {
+        private String usuario;
+        private String password;
+
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
